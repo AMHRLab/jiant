f2ca09ab8c07596bc5b34af2a5998d763d2e0c26
Antonio Laverghetta <A.V.Laverghetta@gmail.com>
2020-05-17 17:16:02 -0400

    initial commit for bertscore

--- ahmrScripts/computeDifficultyMetrics.py
+++ ahmrScripts/computeDifficultyMetrics.py
@@ -8,9 +8,36 @@ import json
 import matplotlib.pyplot as plt
 import numpy as np
 import math
+import logging
+import transformers
 
+from bert_score import BERTScorer
 from nltk.tokenize import word_tokenize
 from sklearn.feature_extraction.text import CountVectorizer
+
+# Helper scripts
+def genPlots(data):
+    # TODO move creating the plots into a separate function
+    print("Generating analysis")
+    # generate a histogram and cdf
+    data = np.asarray(data)
+    data_sorted = np.sort(data)
+    proportional = np.linspace(0,1,len(data),endpoint=False)
+    
+    fig = plt.figure()
+    ax1 = fig.add_subplot(121)
+    ax1.hist(data,bins=70)
+    ax2 = fig.add_subplot(122)
+    ax2.plot(data_sorted,proportional)
+    plt.savefig("./hist.png",bbox_inches='tight')
+    return data_sorted,proportional
+
+def getLine(line):
+    js = json.loads(line)
+    seq = f"{js['obs1']} {js['obs2']} {js['hyp1']} {js['hyp2']}"
+    return seq
+
+
 
 """
 Here, the difficulty is based on the total number of words in the example.
@@ -26,8 +53,7 @@ def sentenceLength():
     with open("./train.jsonl","r") as srcFile:
         print("Getting seq lengths")
         for line in srcFile:
-            js = json.loads(line)
-            seq = f"{js['obs1']} {js['obs2']} {js['hyp1']} {js['hyp2']}"
+            seq = getLine(line)
             # tokensize and get rid of periods
             tokens = word_tokenize(seq)
             tokens = list(filter((".").__ne__,tokens))
@@ -67,13 +93,10 @@ def wordRarity(lowercase=False):
     totalWords = 0
     with open("./train.jsonl","r") as srcFile:
         for line in srcFile:
-            js = json.loads(line)
-            seq = f"{js['obs1']} {js['obs2']} {js['hyp1']} {js['hyp2']}"
-            try:
-                dstFile.writelines(seq.encode("ascii",errors="ignore"))
-            except UnicodeEncodeError as e:
-                print(f"Error: {e}")
+            seq = getLine(line)
+            dstFile.writelines(seq.encode("ascii",errors="ignore"))
             dstFile.write("\n")
+
         dstFile.close()
         dstFile = open("./words.txt", "r")
         # TODO add option for stopwords
@@ -88,8 +111,7 @@ def wordRarity(lowercase=False):
     data = []   # for generating the graphs
     with open("./train.jsonl","r") as srcFile:
         for line in srcFile:
-            js = json.loads(line)
-            seq = f"{js['obs1']} {js['obs2']} {js['hyp1']} {js['hyp2']}"
+            seq = getLine(line)
             tokens = word_tokenize(seq)
             tokens = list(filter((".").__ne__,tokens))
             logProb = 0.0
@@ -115,21 +137,33 @@ def wordRarity(lowercase=False):
 
 
 
-def genPlots(data):
-    # TODO move creating the plots into a separate function
-    print("Generating analysis")
-    # generate a histogram and cdf
-    data = np.asarray(data)
-    data_sorted = np.sort(data)
-    proportional = np.linspace(0,1,len(data),endpoint=False)
-    
-    fig = plt.figure()
-    ax1 = fig.add_subplot(121)
-    ax1.hist(data,bins=70)
-    ax2 = fig.add_subplot(122)
-    ax2.plot(data_sorted,proportional)
-    plt.savefig("./hist.png",bbox_inches='tight')
-    return data_sorted,proportional
+"""
+This approach instead looks at the semantic similarity between h1 and h2 using BertScore
+If BertScore judge them to be similar, the problem is scored harder because it will presumably
+be more difficult to judge which hypothesis is correct
+"""
+def bertScore():
+    with open("./train.jsonl","r") as srcFile:
+        all_F1 = []
+        all_P = []
+        all_R = []
+        scorer = BERTScorer(lang='en',rescale_with_baseline=True)
+        for line in srcFile:
+            js = json.loads(line)
+            # the api expects a list of strings
+            hyp1 = []
+            hyp2 = []
+            hyp1.append(js['hyp1'])
+            hyp2.append(js['hyp2'])          
+            P, R, F1 = scorer.score(hyp1, hyp2, verbose=True)
+            all_F1.append(F1)
+            all_P.append(P)
+            all_R.append(R)
+        
+        print("here")
+
+
+
 
             
 
@@ -137,4 +171,5 @@ def genPlots(data):
 
 # Uncomment the one you want
 # sentenceLength()
-wordRarity()
+# wordRarity()
+bertScore()
